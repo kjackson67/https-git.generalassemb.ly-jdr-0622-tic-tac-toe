1efb37b07cd050b17e2c637545d5ad69d592ea9e
“Korby <jacksonkorbyr@johndeere.com>
2020-07-08 14:34:26 -0500

    tic-tac-toe update

---
+++ tic-tac-toe.js
@@ -0,0 +1,7 @@
+// Establish an element to use for game
+const container = document.querySelector("#game-container");
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+const 
