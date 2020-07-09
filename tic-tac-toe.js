6589aea32b2eefbf5a55f59c5198abc54a503309
“Korby <jacksonkorbyr@johndeere.com>
2020-07-09 00:39:54 -0500

    tic-tac-toe update#2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,41 @@
 // Establish an element to use for game
-const container = document.querySelector("#game-container");
-let gameActive = true;
+let counter = 0;
 let currentPlayer = "X";
 let gameMoves = ["", "", "", "", "", "", "", "", ""];
 
-const 
+const resetBtn = document.querySelector("#game-reset");
+const game = document.querySelector("#game-container"); 
+const square = document.querySelectorAll(".square");
+
+resetBtn.addEventListener(`click`, resetGame);
+
+
+function resetGame() {
+    gameMoves = ["", "", "", "", "", "", "", "", ""];
+    currentPlayer = "X";
+    counter = 0;
+    console.log(resetGame);
+    squares.forEach((square) => {
+        square.innerHTML = "";
+        square.addEventListener("click", square, {once: true });
+    });
+}
+
+function squareEventListener() {
+    gameMoves[this.add] = currentPlayer;
+    counter++;
+    winLogic();
+    // console.log(gameMoves);
+    this.innerHTML = currentPlayer;
+    if(currentPlayer === "X") {
+        currentPlayer = "O";
+    }else {
+        currentPlayer = "X";
+    }
+}
+
+function winLogic() {
+    if (gameMoves[0] === gameMoves[1] && gameMoves[1] === gameMoves[2] && gameMoves[0] !== "") {
+
+    }
+}
